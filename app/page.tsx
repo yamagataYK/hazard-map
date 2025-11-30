6dc3aab0f98c0302a31448202cd1f698416d6b88
Yamagata Kaito <yamagatakaito0921@gmail.com>
2025-12-01 05:03:01 +0900

    クリック時に、ハザードマップへ

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Map from "../components/Map";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <main>
-
-      </main>
+      <p className={styles.title}>
+        自身が発生しました
+      </p>
+      <p className={styles.subtitle}>危険区域について</p>
+      <Link href="/map">
+        <button type="button" className={styles.Btn}>見る</button>
+      </Link>
     </>
   );
 }
--- components/Map.tsx
+++ components/Map.tsx
@@ -1,12 +1,28 @@
 "use client";
 import { useEffect, useState } from "react";
-import { MapContainer, TileLayer, Marker, } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Polygon } from 'react-leaflet';
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
+import { supabase } from "@/lib/supabaseClient";
+
+
+type LatLng = [number, number];
+
+type HazardRow = {
+    id: string;
+    disaster_type: string;
+    damage_level: string;
+    polygon: {
+        type: string;
+        coordinates: any;
+    };
+};
 
 
 export default function Map() {
-    const [pos, setPos] = useState<[number, number] | null>(null);
+    const [pos, setPos] = useState<LatLng | null>(null);
+
+    const [areas, setAreas] = useState<HazardRow[]>([]);
 
     const myIcon = L.icon({
         iconUrl: "/my-location.png",
@@ -21,10 +37,38 @@ export default function Map() {
         });
     }, []);
 
+    useEffect(() => {
+        const fetchAreas = async () => {
+            const { data, error } = await supabase
+                .from("hazard_areas")
+                .select("*");
+
+            if (error) {
+                console.error("Supabase error:", error);
+                return;
+            }
+
+            console.log("hazard_areas from Supabase:", data);
+            if (!data) return;
+
+            setAreas(data as HazardRow[]);
+        };
+
+        fetchAreas();
+    }, []);
+
+    const center: LatLng = pos ?? [34.6937, 135.5023];
+
+    const getColor = (level: string) => {
+        if (level === "high") return "red";
+        if (level === "medium") return "orange";
+        return "yellow";
+    };
+
     return (
         <>
             <MapContainer
-                center={pos ?? [34.6937, 135.5023]} // 大阪駅
+                center={center}
                 zoom={13}
                 style={{ height: "100vh", width: "100%" }}
             >
@@ -33,6 +77,34 @@ export default function Map() {
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
                 {pos && <Marker position={pos} icon={myIcon} />}
+
+
+
+                {areas.map((z) => {
+                    const coords = z.polygon.coordinates;
+
+                    let ring: number[][];
+
+                    if (Array.isArray(coords[0][0])) {
+                        ring = coords[0] as number[][];
+                    } else {
+                        ring = coords as number[][];
+                    }
+                    const positions: LatLng[] = ring.map(
+                        ([lng, lat]) => [lat, lng] as LatLng
+                    );
+                    return (
+                        <Polygon
+                            key={z.id}
+                            positions={positions}
+                            pathOptions={{
+                                color: getColor(z.damage_level),
+                                fillColor: getColor(z.damage_level),
+                                fillOpacity: 0.4,
+                            }}
+                        />
+                    );
+                })}
             </MapContainer>
         </>
     )
