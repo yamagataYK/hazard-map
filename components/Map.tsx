f7e78ae959ca6f90105e18b66d0f6dbf787e2bf2
Yamagata Kaito <yamagatakaito0921@gmail.com>
2025-12-01 00:41:49 +0900

    現在地のマーク

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,12 +1,19 @@
 "use client";
 import { useEffect, useState } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, } from 'react-leaflet';
+import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
 
 export default function Map() {
     const [pos, setPos] = useState<[number, number] | null>(null);
 
+    const myIcon = L.icon({
+        iconUrl: "/my-location.png",
+        iconSize: [30, 30],
+        iconAnchor: [20, 40],
+
+    });
 
     useEffect(() => {
         navigator.geolocation.getCurrentPosition((p) => {
@@ -17,7 +24,7 @@ export default function Map() {
     return (
         <>
             <MapContainer
-                center={pos ?? [34.6937, 135.5023]} // 大阪駅あたり
+                center={pos ?? [34.6937, 135.5023]} // 大阪駅
                 zoom={13}
                 style={{ height: "100vh", width: "100%" }}
             >
@@ -25,7 +32,7 @@ export default function Map() {
                     attribution='&copy; OpenStreetMap contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                {pos && <Marker position={pos} />}
+                {pos && <Marker position={pos} icon={myIcon} />}
             </MapContainer>
         </>
     )
